2e553bc8ba004d8a402ec091da50a83f0789db70
mat43 <mat@mathew.ws>
2024-06-13 21:15:37 -0700

    Fixed bugs in authentication
    Implemented a more efficient way to get client data and use them in specific pages, user will now get automatically logged out if their token is expired, began working on index page.

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -2,18 +2,23 @@ import { useAuthStore } from '~/store/auth'; // import the auth store we just cr
 
 export default defineNuxtRouteMiddleware(async (to) => {
     const { authenticated } = storeToRefs(useAuthStore()); // make authenticated state reactive
-    const { getUserInfo } = useAuthStore(); // check token and store info for front end
+
     const token = useCookie('token'); // get token from cookies
-    const nuxtApp = useNuxtApp();
+
+    interface UserInfo {
+      id: String,
+      firstName: String,
+      lastName: String,
+      email: String,
+      password: String,
+      avatar: String,
+      phoneNumber: BigInteger,
+      city: String,
+      role: String
+    }
   
     if (token.value) {
       // check if value exists
-      if (to?.name !== 'login') {
-        const data = await getUserInfo(token.value);
-        if (data.value != null) {
-          nuxtApp.provide('userInfo', data);
-        }
-      }
       authenticated.value = true; // update the state to authenticated
       } else {
         token.value = null; // clear the token cookie
--- pages/index.vue
+++ pages/index.vue
@@ -2,17 +2,48 @@
 const nuxtApp = useNuxtApp();
 import { useAuthStore } from '~/store/auth';
 const { logUserOut } = useAuthStore();
+const router = useRouter();
+const token = useCookie('token'); // get token from cookies
+const { getUserInfo } = useAuthStore(); // check token and store info for front end
+const data = await getUserInfo(token.value);
 
 const logout = async () => {
-  const router = useRouter();
   await logUserOut();
   router.push('/login');
 };
 
 
-</script>
+const checkIfLoggedIn = computed(() => {
+    if (data.value == null) {
+        logout();
+        return false;
+    } else {
+        return true;
+    }
+});
 
-<template>
-    <h1>{{  nuxtApp.$userInfo }}</h1>
-   <button @click="logout">Logout</button>
+const getTime = computed(() => {
+    var now = new Date().getHours();
+    if (now < 12) {
+        return "morning";
+    } else if (now >= 12 && now < 18) {
+        return "afternoon";
+    } else {
+        return "evening";
+    }
+})
+
+const { authenticated } = storeToRefs(useAuthStore()); // make authenticated state reactive
+
+</script>
+<template> 
+    <div class="bg-loginBackground bg-cover bg-center bg-no-repeat h-full p-5 rounded-md" v-if="checkIfLoggedIn"> <!-- Container Wrapper -->
+        <div class="flex">
+            <div class="text-black bg-transparent bg-white border-2 border-black p-4"> <!-- Hello Box -->
+                <h1>Good {{ getTime }} {{ data.firstName  }}</h1> <!-- Implement get time function for this -->
+            </div>
+        </div>
+        <button @click="logout">Logout</button>
+    </div>
+
 </template>
